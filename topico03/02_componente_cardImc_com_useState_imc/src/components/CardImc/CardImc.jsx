9a7dd7503d96b04ded986c9d3acd86eda1310345
IgorSimim <igorleonsimim@gmail.com>
2025-10-05 15:11:14 -0300

    :sparkles: Add activity 01 requested in topic 03

--- topico03/02_componente_cardImc_com_useState_imc/src/components/CardImc/CardImc.jsx
+++ topico03/02_componente_cardImc_com_useState_imc/src/components/CardImc/CardImc.jsx
@@ -1,20 +1,38 @@
-/* eslint-disable no-unused-vars */
-/* eslint-disable react/prop-types */
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import './style.css';
 
 export default function CardImc({ pessoa }) {
-  const peso = pessoa.peso;
-  const alt = pessoa.altura;
-  const calcImc = () => peso / alt ** 2;
-  const [imc, setImc] = useState(calcImc);
+  const [peso, setPeso] = useState(pessoa.peso);
+  const [altura] = useState(pessoa.altura);
+  const [imc, setImc] = useState(peso / (altura ** 2));
+  const [cor, setCor] = useState('green');
+
+  useEffect(() => {
+    const novoImc = peso / (altura ** 2);
+    setImc(novoImc);
+
+    if (novoImc <= 24.5) {
+      setCor('green');
+    } else if (novoImc > 24.5 && novoImc < 30) {
+      setCor('yellow');
+    } else {
+      setCor('red');
+    }
+  }, [peso, altura]);
+
+  const aumentarPeso = () => setPeso((prev) => prev + 1);
+  const diminuirPeso = () => setPeso((prev) => (prev > 1 ? prev - 1 : prev));
 
   return (
-    <div className="imcCard">
-      <h1>{pessoa.peso}:</h1>
-      <p>Peso: {alt} m</p>
-      <p>Altura: {alt} m</p>
-      <p>Imc: {imc.toFixed(2)}</p>
+    <div className="imcCard" style={{ backgroundColor: cor, borderRadius: '30px', textAlign: 'center', }}>
+      <h2>{pessoa.nome}</h2>
+      <p>Altura: {altura} m</p>
+      <p>Peso: {peso} kg</p>
+      <div className="botoes-container">
+        <button onClick={diminuirPeso}>-</button>
+        <button onClick={aumentarPeso}>+</button>
+      </div>
+      <p>IMC: {imc.toFixed(2)}</p>
     </div>
   );
 }
